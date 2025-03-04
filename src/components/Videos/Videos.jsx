fe1957f6814f459a6a2c6c3b4fa5bd853e296ada
Victoria <itsfoxiefo@gmail.com>
2025-03-04 02:01:10 +0100

    added React Lite YouTube Embed for Videos component

--- src/components/Videos/Videos.jsx
+++ src/components/Videos/Videos.jsx
@@ -1,5 +1,6 @@
 import s from './styles.module.scss';
-
+import LiteYouTubeEmbed from 'react-lite-youtube-embed';
+import 'react-lite-youtube-embed/dist/LiteYouTubeEmbed.css';
 import GameButton from './../ui/GameButton/GameButton';
 
 export default function Videos() {
@@ -9,25 +10,14 @@ export default function Videos() {
         Ready to test your nerves?
       </h1>
       <div className={s.videosContainer}>
-        <iframe
-          className={s.videosContainerVideo}
-          src="https://www.youtube.com/embed/tvTsUs0n9zw?si=Q6Sd9tHMU6jwle1V"
-          title="YouTube video player"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-          allowFullScreen
-        ></iframe>
+        {/* Using LiteYouTubeEmbed instead of iframe */}
+        <LiteYouTubeEmbed id="tvTsUs0n9zw" title="YouTube video player" />
         <p className={s.videosDescriptionParagraphMobile}>
           Step into the darkness and feel the fear unfold. Watch our game
           trailers and experience the horror coming to life… Click and watch-if
           you dare.
         </p>
-        <iframe
-          className={s.videosContainerVideo}
-          src="https://www.youtube.com/embed/r6WqezmOMc4?si=CI9wa_01Vpq5EYNQ"
-          title="YouTube video player"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-          allowFullScreen
-        ></iframe>
+        <LiteYouTubeEmbed id="r6WqezmOMc4" title="YouTube video player" />
       </div>
       <div className={s.videosDescription}>
         <p className={s.videosDescriptionParagraph}>
